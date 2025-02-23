0a92e65b9e194ddb3581d063c77f90ca00001f08
AarkayITsolutions <alihassanali119683@gmail.com>
2025-02-23 19:35:34 +0500

    adding files

---
+++ loops.py
@@ -0,0 +1,20 @@
+"""
+Loops in Python
+
+Loops are used to execute a block of code multiple times.
+Python provides two types of loops:
+1. for loop
+2. while loop
+"""
+
+# Example of a for loop
+print("For loop example:")
+for i in range(5):  # Iterates from 0 to 4
+    print("Iteration:", i)
+
+# Example of a while loop
+print("\nWhile loop example:")
+count = 0
+while count < 5:
+    print("Iteration:", count)
+    count += 1
