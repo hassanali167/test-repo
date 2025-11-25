5d4fa7229d19682dba5d0bb72fe4440d5abec132
Hassan Ali <b22f1335se021@fecid.paf-iast.edu.pk>
2025-11-25 21:27:22 +0500

    Update args_and_kwargs.py

--- args_and_kwargs.py
+++ args_and_kwargs.py
@@ -1,6 +1,7 @@
 """
 Topic: *args and **kwargs in Python
 
+
 Explanation:
 `*args` allows passing multiple positional arguments to a function.
 `**kwargs` allows passing multiple keyword arguments.
